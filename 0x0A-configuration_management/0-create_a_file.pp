0d5e6f5c0a3ca2741c63eb490fbb5752006029a1
MarB-T <danmarbet@gmail.com>
2023-08-26 04:58:33 +0300

    First Puppet file creation lint complience

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,6 @@
 #create a file school
 file {'/tmp/school':
-  ensure  => file,
-  path    => '/tmp/holberton',
+  ensure  => 'file',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
