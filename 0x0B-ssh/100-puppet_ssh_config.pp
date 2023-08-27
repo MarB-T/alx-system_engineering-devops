b93c9276d16c5baac8e4ed153b6bdff1837c9340
MarB-T <danmarbet@gmail.com>
2023-08-27 22:01:24 +0300

    change ssh configuration using puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,13 @@
-file_line { 'Turn off password auth':
+#Puppet script to change ssh configuration
+file_line { 'Turn off password authentication':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => 'PasswordAuthentication no',
-  match  => '^#?\s*PasswordAuthentication',
 }
 
 file_line { 'Declare identity change':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => 'IdentityFile ~/.ssh/school',
-  match  => '^#?\s*IdentityFile',
 }
 
