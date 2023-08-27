db1f7e453fbb4ac2c86fb528048914ba6075c307
MarB-T <danmarbet@gmail.com>
2023-08-27 22:03:10 +0300

    correct indentation

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,11 +3,13 @@ file_line { 'Turn off password authentication':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => 'PasswordAuthentication no',
+  match  => '^#?\s*PasswordAuthentication',
 }
 
 file_line { 'Declare identity change':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => 'IdentityFile ~/.ssh/school',
+  match  => '^#?\s*IdentityFile'
 }
 
