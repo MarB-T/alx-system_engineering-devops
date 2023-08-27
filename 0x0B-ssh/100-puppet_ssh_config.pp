9a1f9f4c1c3f466688d2b305309088552d76d809
MarB-T <danmarbet@gmail.com>
2023-08-27 22:04:21 +0300

    remove extra config lines

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,13 +3,11 @@ file_line { 'Turn off password authentication':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => 'PasswordAuthentication no',
-  match  => '^#?\s*PasswordAuthentication',
 }
 
 file_line { 'Declare identity change':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => 'IdentityFile ~/.ssh/school',
-  match  => '^#?\s*IdentityFile'
 }
 
