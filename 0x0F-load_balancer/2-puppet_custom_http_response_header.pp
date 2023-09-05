ef5d221bf2c4db05b62eb62f8eaa7975b5f6f719
MarB-T <danmarbet@gmail.com>
2023-09-05 05:53:30 +0300

    update puppet manifest

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,1 +1,33 @@
-# Installing Nginx
+# Install Nginx
+class { 'nginx':
+  ensure => 'installed',
+}
+
+# Define a custom fact to retrieve the server's hostname
+facts['custom_http_header'] = $::hostname
+
+# Define an Nginx server block with a custom HTTP header
+file { '/etc/nginx/sites-available/custom_http_header':
+  ensure  => 'file',
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+  content => template('my_module/custom_http_header.erb'),
+}
+
+# Create a symbolic link to enable the custom server block
+file { '/etc/nginx/sites-enabled/custom_http_header':
+  ensure => 'link',
+  target => '/etc/nginx/sites-available/custom_http_header',
+  notify => Service['nginx'],
+}
+
+# Ensure Nginx service is running and enabled
+service { 'nginx':
+  ensure  => 'running',
+  enable  => true,
+  require => [
+    File['/etc/nginx/sites-enabled/custom_http_header'],
+  ],
+}
+
