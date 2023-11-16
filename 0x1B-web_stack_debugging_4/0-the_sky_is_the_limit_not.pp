9abaa2ed47a6ec8abed5ac757f349b111255a823
MarB-T <danmarbet@gmail.com>
2023-11-16 22:32:05 +0300

    Increase trafic for nginx server

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,11 @@
+# Solve failing requests by increasing traffic an Nginx server can handle
+
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+exec { 'nginx-restart':
+  command => 'service nginx restart',
+  path    => '/etc/init.d/'
+}
