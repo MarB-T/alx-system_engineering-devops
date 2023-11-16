0b72a92a61bb1c0cc1b839007fdb0fa9f4731b5b
MarB-T <danmarbet@gmail.com>
2023-11-16 22:41:50 +0300

    Increase trafic for nginx server correction

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 # Solve failing requests by increasing traffic an Nginx server can handle
 
 exec { 'fix--for-nginx':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  command => 'sed -ie "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
 }
 
